7f5ccee71c86c7c0324a91df0aca4de46e9588d0
Daniel Victor <daniel.varezende@senacsp.edu.br>
2021-08-12 18:08:28 -0300

    Update 12/08/21

--- src/pages/Alunos.js
+++ src/pages/Alunos.js
@@ -1,7 +1,51 @@
-import React from 'react'
+import React, {Component} from 'react'
 import '../components/Navbar.css';
+import api from '../api'
 
-export default function Alunos()  {
+class App extends Component{
+
+    state = {
+      alunos: [],
+    }
+  
+  async componentDidMount(){
+    const response = await api.get('/alunos')
+   // console.log(response.data);
+    this.setState({alunos: response.data});
+  }
+  
+    render(){
+  const {alunos} = this.state;
+  console.log(alunos);
+      return(
+        <div>
+            <center>
+                <br/>
+        <h1>Lista de Alunos</h1>
+        <br/>
+        <hr/>
+        <br/>        
+        <ul style={{listStyleType: "nome", margin:0, padding:0}}>
+        
+          {alunos.map(aluno => (
+            <li key={aluno._id}><br/>
+            <div className='elemento'>
+            <h3>Nome: {aluno.nome}</h3>            
+              <p>E-mail: {aluno.email} </p>
+              </div>
+            </li>
+          ))}          
+        </ul>        
+        </center>
+        </div>
+      );
+    };
+  };
+
+  export default App;
+
+
+/* export default function Alunos()  {
     return (
         
         <html>
@@ -35,4 +79,4 @@ export default function Alunos()  {
           
        
     )
-}
+} */
